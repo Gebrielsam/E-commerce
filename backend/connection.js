722c865d84d575675eb3386b16d14151e297d029
Onah Obinna Samuel <samgb042@gmail.com>
2022-12-03 09:03:21 +0100

    first commit

---
+++ backend/connection.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.swiuune.mongodb.net/e-commerce-pro?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionStr, {useNewUrlParser: true})
+.then(() => console.log('connected to mongodb'))
+.catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+})
